f99d2a3dff56ab7621d251f510b634aa6f2591f4
yousif <138237408+yousifj129@users.noreply.github.com>
2024-08-19 21:48:58 +0300

    Update Program.cs

--- VimSymb/Program.cs
+++ VimSymb/Program.cs
@@ -1,7 +1,4 @@
-﻿Expression expr1 = new Expression();
-expr1.ParseExpression("5*x + 2*x");
-Console.WriteLine("5x + 2x = " + expr1.Simplify().PrintExpression());
-
+﻿
 Expression expr2 = new Expression();
 expr2.ParseExpression("5*x * 5*x");
 Console.WriteLine("5x * 2x = " + expr2.Simplify().PrintExpression());
